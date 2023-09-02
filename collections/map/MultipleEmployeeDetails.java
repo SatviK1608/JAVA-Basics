da42188d29c10432ffdb8502f62607e37e7dfe95
SatviK1608 <satvik1608@gmail.com>
2023-09-02 21:31:35 +0530

    java

--- collections/map/MultipleEmployeeDetails.java
+++ collections/map/MultipleEmployeeDetails.java
@@ -23,7 +23,9 @@ public class MultipleEmployeeDetails {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int totalEmployee=2;
+		
+		
+		/*int totalEmployee=2;
 		Map<String, List<String>> m=new HashMap<>();
 		
 		for(int i=0;i<totalEmployee;i++){
@@ -36,7 +38,24 @@ public class MultipleEmployeeDetails {
 		
 		for(Map.Entry<String,List<String>> entry:m.entrySet()){
 			System.out.println(entry.getKey()+" - "+entry.getValue());
-		}
+		}*/
+		
+		
+		
+		//STORE ALL LIST VALUES AS "Arrays.asList()" IN A SINGLE LINE FOR EACG KEY
+		
+		Map<String,List<String>> _objM=new  HashMap<>();
+		
+		List<String> _objL=new ArrayList<>();
+		
+		_objL=Arrays.asList("satvik","hr","12lakh");
+		_objM.put("1315", _objL);
+		
+		_objL=Arrays.asList("harshit","jnr emp","6lakh");
+		_objM.put("1325", _objL);
+		
+		System.out.println(_objM);
+		
 
 	}
 
